09ab362113b6022c9e92b1df1acfd7705a8704cf
HARUKA ito <haruka_ito@keio.jp>
2020-09-25 15:10:20 +0900

    タイトルと著者の表示まで

--- API_practice/ViewController.swift
+++ API_practice/ViewController.swift
@@ -17,16 +17,22 @@ class ViewController: UIViewController, UITableViewDataSource {
     //tableviewを宣言
     @IBOutlet var table: UITableView!
     
-    fileprivate var articles: [Qiita] = []
+    var articles: [Qiita] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+       
         QiitaAPI().fetchArticle { (qiitaResults) in
-            print(qiitaResults[0].user)
+            print(qiitaResults[0].title)
+            
+            self.articles = qiitaResults
+            DispatchQueue.main.async {
+                self.table.reloadData()
+            }
+            
         }
-        
         table.dataSource = self
         
 
@@ -34,16 +40,24 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     //セルの数を設定
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return articles.count
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        let article = articles[indexPath.row]
+        //cell?.textLabel?.text = article.title
+        //cell?.detailTextLabel?.text = article.user.name
+        tableView.rowHeight = 60
         
-        cell?.textLabel?.text = "テスト"
-        
-        return cell!
+        let labelTitle = cell.viewWithTag(1) as! UILabel
+        labelTitle.text = article.title
+
+        let labelUser = cell.viewWithTag(2) as! UILabel
+        labelUser.text = article.user.name
+        return cell
     }
 }
 
